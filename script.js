f5d1929f103499f88efa923dae0c5c697d5a77cd
Clark Wimberly <clark@clarklab.net>
2025-04-21 03:09:42 -0500

    style

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var movies = [
 ];
 
 var currentMovieIndex = 0;
-const startDate = new Date('2025-04-20');
+const startDate = new Date('2025-04-23');
 
 function getWatchDate(movieIndex) {
   const watchDate = new Date(startDate);
@@ -50,17 +50,22 @@ function displayMovie(movieData, index) {
         </p>
       </div>
       <div class='px-6 text-gray-500 flex flex-col flex-grow *:flex-grow'>
-        <p class="line-clamp-3">${movieData.Plot}</p>
+        <p class="line-clamp-3 text-[#746BD9]">${movieData.Plot}</p>
         <div class='flex !flex-grow-0 justify-between gap-6 border-t pt-4 mt-4'>
           <p class='text-gray-500' title='Watch Date'>
-            <svg class='inline w-5 h-5 relative -top-0.5' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor'>
-              <path stroke-linecap='round' stroke-linejoin='round' d='M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5' />
-            </svg>
-            &nbsp;<strong>Watch on:</strong> ${formattedDate}
+          <svg class='inline w-5 h-5 relative -top-0.5'  width="100pt" height="100pt" version="1.1" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+          <g>
+            <path d="m33.332 16.668c-2.293 0-4.168 1.875-4.168 4.168v6.25h8.332v-6.25c0.003906-2.293-1.8711-4.168-4.1641-4.168z"/>
+            <path d="m70.832 27.082v4.168c0 2.293-1.875 4.168-4.168 4.168-2.293 0-4.168-1.875-4.168-4.168v-4.168h-24.996v4.168c0 2.293-1.875 4.168-4.168 4.168s-4.168-1.875-4.168-4.168v-4.168h-12.496v56.25h66.668l-0.003907-56.25zm4.168 47.918h-50v-35.418h50z"/>
+            <path d="m66.668 16.668c-2.293 0-4.168 1.875-4.168 4.168v6.25h8.332v-6.25c0-2.293-1.875-4.168-4.1641-4.168z"/>
+            <path d="m45.832 70.418-11.25-11.25 5.8359-5.8359 5.4141 5.418 13.75-13.75 5.8359 5.832z"/>
+          </g>
+          </svg>
+            &nbsp;<strong>Watch:</strong> ${formattedDate}
           </p>
           <p class='text-gray-500' title='IMDb Rating'>
-            <svg class='inline w-5 h-5 relative -top-0.5' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor'>
-              <path stroke-linecap='round' stroke-linejoin='round' d='M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z' />
+            <svg class='inline w-5 h-5 relative -top-0.5' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='#746BD9'>
+              <path fill-rule='evenodd' d='M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z' clip-rule='evenodd' />
             </svg>
             &nbsp;${movieData.imdbRating}
           </p>
